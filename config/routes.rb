ceabfbeab93434d19164a2714c02d4555dc4e064
niri4 <nirankarsingh247@gmail.com>
2018-01-13 03:30:22 +0530

    add skip signup  in devise add in users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "home#index"
+  devise_for :users, :skip => [:registration]
+  resources :users
+  root to: "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
