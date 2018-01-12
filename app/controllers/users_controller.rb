d605316a510e3c6a820858b7768e347a7f6776f8
niri4 <nirankarsingh247@gmail.com>
2018-01-13 03:48:31 +0530

    implemented enum for roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
   def new
     @user = User.new
+    @roles = User.roles
   end
 
   def show
@@ -23,6 +24,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email,:name,:password,:password_confirmation)
+    params.require(:user).permit(:email,:name,:password,:password_confirmation,:role)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum roles: {manager: 0, employer: 1, hr: 2,vp: 3}
 end
