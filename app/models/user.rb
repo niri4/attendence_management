9deb0206c931fc7e22b854941b45365747f30ad4
niri4 <nirankarsingh247@gmail.com>
2018-01-13 03:53:51 +0530

    implementation validation on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   enum roles: {manager: 0, employer: 1, hr: 2,vp: 3}
+  validates_presence_of :role,:name,:email,:password,:password_confirmation
 end
